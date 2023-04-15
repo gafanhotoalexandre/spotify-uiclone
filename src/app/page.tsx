79ed490c60df53c0765e18992951287882b51968
Alexandre Martins <alexandrevmartinsdelima@gmail.com>
2023-04-15 15:50:42 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <aside></aside>
+        <main>Spotify UI Clone</main>
+      </div>
+      <footer></footer>
+    </div>
+  )
+}
